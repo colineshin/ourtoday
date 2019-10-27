74db2cf61100c6ce629cd659051407af2cefc5f4
colineshin <jiwonshin.js@gmail.com>
2019-10-27 11:33:16 +0900

    Initial commit

---
+++ lib/tab_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:ourtoday/account_page.dart';
+import 'package:ourtoday/write_page.dart';
+import 'package:ourtoday/search_page.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  _TabPageState createState() => _TabPageState();
+}
+
+class _TabPageState extends State<TabPage> {
+  int _selectedIndex = 0;
+
+  List _pages = [
+    SearchPage(),
+    WritePage(),
+    AccountPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: _pages[_selectedIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.red,
+          onTap: _onItemTapped,
+          currentIndex: _selectedIndex,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.search), title: Text('Ourdays')),
+            BottomNavigationBarItem(icon: Icon(Icons.create), title: Text('Write')),
+            BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('dMydays')),
+          ]),
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
